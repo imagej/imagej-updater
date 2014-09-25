imagej/imagej-updater

commit c693611d7cb92e1d01080d6838512b88d134f39d

Make edit-update-site less strict
When we edit an update site, we already have to provide all the
information required to add it should an update site of the specified
name not have been added yet.

Be nice and just auto-add the site.

This addresses https://github.com/imagej/imagej-updater/issues/21
partially.

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

src/main/java/net/imagej/updater/CommandLine.java
@@ -977,8 +977,6 @@ public class CommandLine {
 				throw die("Site '" + name + "' was already added!");
 			files.addUpdateSite(name, url, sshHost, uploadDirectory, 0l);
 		} else {
-			if (site == null)
-				throw die("Site '" + name + "' was not yet added!");
 			site.setURL(url);
 			site.setHost(sshHost);
 			site.setUploadDirectory(uploadDirectory);

commit e371e7dcfa02b8ebc925bf2e7c8e92bdd91cefa7

Updater (cmd-line --edit-update-site): *actually* auto-add sites
This is a bug that was introduced by yours truly in c693611d...

Noticed by Curtis Rueden (actually, Julis Jenkins).

Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

src/main/java/net/imagej/updater/CommandLine.java
@@ -1247,7 +1247,7 @@ public class CommandLine {
 			final boolean add) {
 		ensureChecksummed();
 		final UpdateSite site = files.getUpdateSite(name, true);
-		if (add) {
+		if (site == null || add) {
 			if (site != null)
 				throw die("Site '" + name + "' was already added!");
 			files.addUpdateSite(name, url, sshHost, uploadDirectory, 0l);