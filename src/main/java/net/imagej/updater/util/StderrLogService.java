005315caf18372d5ad0a4038e6fec23f723db304
Curtis Rueden <ctrueden@wisc.edu>
2018-03-01 07:35:40 -0600

    Update parent to pom-scijava 19.1.1
    And address backwards incompatibilities in the LogService interface.

--- src/main/java/net/imagej/updater/util/StderrLogService.java
+++ src/main/java/net/imagej/updater/util/StderrLogService.java
@@ -31,11 +31,8 @@
 
 package net.imagej.updater.util;
 
-import org.scijava.Context;
-import org.scijava.NullContextException;
-import org.scijava.Prioritized;
-import org.scijava.log.LogService;
-import org.scijava.plugin.PluginInfo;
+import org.scijava.log.AbstractLogService;
+import org.scijava.log.LogMessage;
 
 /**
  * Deprecated; do not use.
@@ -48,39 +45,10 @@ import org.scijava.plugin.PluginInfo;
  * @deprecated Use {@link org.scijava.log.StderrLogService} instead.
  */
 @Deprecated
-public class StderrLogService implements LogService {
-	@Override public void initialize() { }
-	@Override public void registerEventHandlers() { }
-	@Override public void dispose() { }
-	@Override public Context context() { throw new NullContextException(); }
-	@Override public Context getContext() { return null; }
-	@Override public void setContext(Context context) { }
-	@Override public double getPriority() { return 0; }
-	@Override public void setPriority(double priority) { }
-	@Override public int compareTo(Prioritized o) { return 0; }
-	@Override public PluginInfo<?> getInfo() { return null; }
-	@Override public void setInfo(PluginInfo<?> info) { }
-	@Override public void debug(Object msg) { System.err.println(msg); }
-	@Override public void debug(Throwable t) { t.printStackTrace(); }
-	@Override public void debug(Object msg, Throwable t) { debug(msg); debug(t); }
-	@Override public void error(Object msg) { debug(msg); }
-	@Override public void error(Throwable t) { debug(t); }
-	@Override public void error(Object msg, Throwable t) { debug(msg, t); }
-	@Override public void info(Object msg) { debug(msg); }
-	@Override public void info(Throwable t) { debug(t); }
-	@Override public void info(Object msg, Throwable t) { debug(msg, t); }
-	@Override public void trace(Object msg) { debug(msg); }
-	@Override public void trace(Throwable t) { debug(t); }
-	@Override public void trace(Object msg, Throwable t) { debug(msg, t); }
-	@Override public void warn(Object msg) { debug(msg); }
-	@Override public void warn(Throwable t) { debug(t); }
-	@Override public void warn(Object msg, Throwable t) { debug(msg, t); }
-	@Override public boolean isDebug() { return true; }
-	@Override public boolean isError() { return true; }
-	@Override public boolean isInfo() { return true; }
-	@Override public boolean isTrace() { return true; }
-	@Override public boolean isWarn() { return true; }
-	@Override public int getLevel() { return TRACE; }
-	@Override public void setLevel(int level) { }
-	@Override public void setLevel(String classOrPackageName, int level) { }
+public class StderrLogService extends AbstractLogService {
+
+	@Override
+	protected void messageLogged(LogMessage message) {
+		System.err.print(message);
+	}
 }
